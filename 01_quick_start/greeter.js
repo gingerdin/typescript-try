0f5adb1f257e0346ddf42e0e1f02dcdccf7b76ca
Denis Levchuk <dliauchuk@exigeninsurance.com>
2017-01-05 13:51:27 +0300

    quick start guide

---
+++ 01_quick_start/greeter.js
@@ -0,0 +1,15 @@
+var Student = (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+/*var user = [0, 1 , 2];*/
+document.body.innerHTML = greeter(user);
